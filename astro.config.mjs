e377b38e8a7ef277c84561c1ece32940008e53e9
Jordi <jllahi@gmail.com>
2024-01-08 15:20:40 +0100

    Add ImageTools

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
+import { astroImageTools } from 'astro-imagetools'
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,5 +13,6 @@ export default defineConfig({
 				heroicons: ['sun', 'moon'],
 			},
 		}),
+		astroImageTools,
 	],
 })
