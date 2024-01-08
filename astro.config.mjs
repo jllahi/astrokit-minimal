b199886bf7ce55fc1e921c59df4e7b90e8882765
Jordi <jllahi@gmail.com>
2024-01-08 16:55:44 +0100

    Update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
-import { astroImageTools } from 'astro-imagetools'
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,6 +12,5 @@ export default defineConfig({
 				heroicons: ['sun', 'moon'],
 			},
 		}),
-		astroImageTools,
 	],
 })
