36f3255d53e3903957e0f74a12959b9128cd11f2
Jordi <jllahi@gmail.com>
2024-01-19 19:21:42 +0100

    Update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
 import debugcss from 'astro-debugcss'
+import { tailwindConfigViewer } from 'astro-tailwind-config-viewer'
 import mkcert from 'vite-plugin-mkcert'
+import metaTags from 'astro-meta-tags'
 
 // https://astro.build/config
 export default defineConfig({
@@ -15,6 +17,8 @@ export default defineConfig({
 			},
 		}),
 		debugcss(),
+		tailwindConfigViewer(),
+		metaTags(),
 	],
 	vite: {
 		plugins: [mkcert()],
