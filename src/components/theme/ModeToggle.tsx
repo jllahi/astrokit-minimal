2579a639103f6c35b000776c6c728238d71656fb
jllahi <1439075+jllahi@users.noreply.github.com>
2024-05-27 02:51:50 +0200

    Refactor code

--- src/components/theme/ModeToggle.tsx
+++ src/components/theme/ModeToggle.tsx
@@ -10,7 +10,9 @@ import {
 } from '@/components/ui/dropdown-menu'
 
 export function ModeToggle() {
-  const [theme, setThemeState] = React.useState<'theme-light' | 'dark' | 'system'>('theme-light')
+  const [theme, setThemeState] = React.useState<
+    'theme-light' | 'dark' | 'system'
+  >('theme-light')
 
   React.useEffect(() => {
     const isDarkMode = document.documentElement.classList.contains('dark')
@@ -21,7 +23,8 @@ export function ModeToggle() {
   React.useEffect(() => {
     const isDark =
       theme === 'dark' ||
-      (theme === 'system' && window.matchMedia('(prefers-color-scheme: dark)').matches)
+      (theme === 'system' &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches)
     document.documentElement.classList[isDark ? 'add' : 'remove']('dark')
     // console.log('THEME REACT: isDark useEffect executed!')
   }, [theme])
@@ -36,9 +39,15 @@ export function ModeToggle() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setThemeState('theme-light')}>Light</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setThemeState('dark')}>Dark</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setThemeState('system')}>System</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setThemeState('theme-light')}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setThemeState('dark')}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setThemeState('system')}>
+          System
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   )
