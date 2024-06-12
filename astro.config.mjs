c584586a6a779db518e4522a67b8837634e2647c
jllahi <1439075+jllahi@users.noreply.github.com>
2024-06-12 02:34:25 +0200

    Refactor code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,24 @@
 import tailwind from '@astrojs/tailwind'
-import debugcss from 'astro-debugcss'
 import icon from 'astro-icon'
-import { defineConfig } from 'astro/config'
-// import mkcert from 'vite-plugin-mkcert'
-import AstroDevtoolbarTailwind from 'astro-devtoolbar-tailwind'
 import metaTags from 'astro-meta-tags'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-	// compressHTML: false,
-	integrations: [
-		tailwind({
-			// applyBaseStyles: false,
-		}),
-		icon({
-			iconDir: 'src/assets/icons',
-			include: {
-				heroicons: ['sun', 'moon'],
-			},
-		}),
-		debugcss(),
-		metaTags(),
-		AstroDevtoolbarTailwind(),
-	],
-	// vite: {
-	// 	plugins: [mkcert()],
-	// },
+  // compressHTML: false,
+  integrations: [
+    tailwind({
+      // applyBaseStyles: false,
+    }),
+    icon({
+      iconDir: 'src/assets/icons',
+      include: {
+        heroicons: ['sun', 'moon'],
+      },
+    }),
+    metaTags(),
+  ],
+  // vite: {
+  // 	plugins: [mkcert()],
+  // },
 })
