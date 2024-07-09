c22077f2ff3fe436f4c738813abedf8a06714f13
jllahi <1439075+jllahi@users.noreply.github.com>
2024-07-09 19:41:09 +0200

    Update packages

---
+++ eslint.config.mjs
@@ -0,0 +1,128 @@
+import globals from "globals";
+import parser from "astro-eslint-parser";
+import tsParser from "@typescript-eslint/parser";
+import typescriptEslint from "@typescript-eslint/eslint-plugin";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+import { includeIgnoreFile } from "@eslint/compat";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+const gitignorePath = path.resolve(__dirname, ".gitignore");
+
+export default [{
+    ignores: [
+        "**/dist/",
+        "**/.astro/",
+        "**/node_modules/",
+        "**/npm-debug.log*",
+        "**/yarn-debug.log*",
+        "**/yarn-error.log*",
+        "**/pnpm-debug.log*",
+        "**/.env",
+        "**/.env.production",
+        "**/.DS_Store",
+        "**/.github",
+        "**/.changeset",
+        "**/pnpm-lock.yaml",
+    ],
+}, includeIgnoreFile(gitignorePath), ...compat.extends("eslint:recommended", "plugin:astro/recommended"), {
+    languageOptions: {
+        globals: {
+            ...globals.node,
+            ...globals.browser,
+        },
+
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+
+    rules: {
+        "no-mixed-spaces-and-tabs": "off",
+    },
+}, {
+    files: ["**/*.astro"],
+
+    languageOptions: {
+        parser: parser,
+        ecmaVersion: 5,
+        sourceType: "script",
+
+        parserOptions: {
+            parser: "@typescript-eslint/parser",
+            extraFileExtensions: [".astro"],
+        },
+    },
+
+    rules: {},
+}, ...compat.extends("plugin:@typescript-eslint/recommended").map(config => ({
+    ...config,
+    files: ["**/*.ts"],
+})), {
+    files: ["**/*.ts"],
+
+    languageOptions: {
+        parser: tsParser,
+    },
+
+    rules: {
+        "@typescript-eslint/no-unused-vars": ["error", {
+            argsIgnorePattern: "^_",
+            destructuredArrayIgnorePattern: "^_",
+        }],
+
+        "@typescript-eslint/no-non-null-assertion": "off",
+    },
+}, {
+    files: ["**/*.d.ts"],
+
+    rules: {
+        "@typescript-eslint/triple-slash-reference": "off",
+    },
+}, ...compat.extends(
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+).map(config => ({
+    ...config,
+    files: ["**/*.tsx"],
+})), {
+    files: ["**/*.tsx"],
+
+    plugins: {
+        "@typescript-eslint": typescriptEslint,
+    },
+
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: 5,
+        sourceType: "script",
+
+        parserOptions: {
+            tsconfigRootDir: "/home/jllahi/Code/Projects/astrokit-minimal",
+            project: ["./tsconfig.json"],
+        },
+    },
+
+    rules: {
+        "@typescript-eslint/no-unused-vars": ["error", {
+            argsIgnorePattern: "^_",
+            destructuredArrayIgnorePattern: "^_",
+        }],
+
+        "@typescript-eslint/no-non-null-assertion": "off",
+    },
+}, {
+    files: ["**/*.astro/*.js", "*.astro/*.js"],
+
+    languageOptions: {
+        parser: tsParser,
+    },
+}];
